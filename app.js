e42c8aa4f98404a22219f419d2802daaf3bbe0ac
laptopcerveza <joseperezprogramming@outlook.com>
2021-02-23 13:40:37 -0500

    small change just to reload server

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 mongoose.connect(DB_URL);
 
 // Set access control response header
+//check route
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
